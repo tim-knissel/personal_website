0ace41aaa9ec4544b92bda8708ce59e6eb888578
TimKnissel <tim.knissel@hotmail.de>
2024-02-11 00:56:49 +0100

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -54,49 +54,6 @@ modalCloseBtn.addEventListener("click", testimonialsModalFunc);
 overlay.addEventListener("click", testimonialsModalFunc);
 
 
-// here new
-
-// testimonials variables
-const serviceItem = document.querySelectorAll("[data-service-item]");
-//const modalContainer = document.querySelector("[data-modal-container]");
-//const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
-//const overlay = document.querySelector("[data-overlay]");
-
-// modal variable
-//const modalImg = document.querySelector("[data-modal-img]");
-//const modalTitle = document.querySelector("[data-modal-title]");
-//const modalText = document.querySelector("[data-modal-text]");
-
-// modal toggle function
-//const serviceModalFunc = function () {
-//  modalContainer.classList.toggle("active");
-//  overlay.classList.toggle("active");
-//}
-
-// add click event to all modal items
-for (let i = 0; i < serviceItem.length; i++) {
-
-  serviceItem[i].addEventListener("click", function () {
-
-    modalImg.src = this.querySelector("[data-service-icon]").src;
-    //modalImg.alt = this.querySelector("[data-service-icon]").alt;
-    modalTitle.innerHTML = this.querySelector("[data-service-title]").innerHTML;
-    modalText.innerHTML = this.querySelector("[data-service-text]").innerHTML;
-
-    testimonialsModalFunc();
-
-  });
-
-}
-
-// add click event to modal close button
-//modalCloseBtn.addEventListener("click", serviceModalFunc);
-//overlay.addEventListener("click", serviceModalFunc);
-
-// until here new
-
-
-
 // custom select variables
 const select = document.querySelector("[data-select]");
 const selectItems = document.querySelectorAll("[data-select-item]");
